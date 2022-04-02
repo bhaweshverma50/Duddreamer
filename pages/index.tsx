c2e9f802136d902180b51be07ac9adfc8a471e9f
Bhawesh <bhaweshverma50@gmail.com>
2022-04-02 21:50:53 +0530

    added styles to post page

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home({ posts }: Props) {
           <h1 className="max-w-l font-serif text-4xl font-medium text-slate-800 md:text-6xl md:text-5xl">
             DudDreamer: Why dream alone when you can share it with others
           </h1>
-          <h2 className="text-md max-w-l pb-6 font-serif md:pb-1 md:text-xl">
+          <h2 className="text-md max-w-l pb-6 font-serif md:pb-4 md:text-xl">
             Ever got curious about what you dreamt last light or if your dream
             means anything? Struggling to remember your dreams from past?
             Wondering if others had a same dream as you? Well, here you can
@@ -36,21 +36,35 @@ export default function Home({ posts }: Props) {
           alt="dream-logo"
         />
       </div>
-      <div>
+      <p className="mx-8 my-5 text-2xl font-bold">Posts</p>
+      <div className="w-100 mx-8 grid grid-cols-1 sm:grid-cols-2 sm:gap-5 lg:grid-cols-3">
         {posts.map((post) => (
           <Link href={`/post/slug=${post.slug.current}`} key={post._id}>
-            <div>
-              <img
-                src={urlFor(post.mainImage).width(600).url()!}
-                alt="banner-image"
-              />
+            <div className="my-2 overflow-hidden rounded-lg border-2 border-yellow-500 bg-gray-600 text-white">
               <div>
-                <p>{post.title}</p>
-                <p>
-                  {post.description} by {post.author.name}
-                </p>
+                <img
+                  className="h-60 w-full object-cover"
+                  src={urlFor(post.mainImage).url()!}
+                  alt="banner-image"
+                />
               </div>
-              <img src={urlFor(post.author.image).width(50).url()!} alt="" />
+              <div className="px-4 py-3">
+                <p className="text-xl font-medium">{post.title}</p>
+                <p className="pt-1 text-[1rem] font-extralight line-clamp-4">
+                  {post.description}
+                </p>
+                <div className="align-center flex gap-3 pt-6 pb-2 text-sm font-thin text-slate-300">
+                  <img
+                    className="h-8 w-8 rounded-sm border-2 border-slate-900"
+                    src={urlFor(post.author.image).width(50).url()!}
+                    alt=""
+                  />
+                  <div className="flex items-center justify-center">
+                    Post by {post.author.name} on{' '}
+                    {new Date(post._createdAt).toLocaleDateString()}
+                  </div>
+                </div>
+              </div>
             </div>
           </Link>
         ))}
@@ -61,7 +75,7 @@ export default function Home({ posts }: Props) {
 
 export const getServerSideProps = async () => {
   const query = `*[_type=="post"]{
-  _id, title,
+  _id, title, _createdAt,
   author -> {
   name,
   image
